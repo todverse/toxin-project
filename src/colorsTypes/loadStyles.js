20cf7293b8b7be9d06fca73b496d73141e6538b4
todverse <todverse0@gmail.com>
2021-11-03 14:34:41 +0400

    rename paths

--- src/colorsTypes/loadStyles.js
+++ src/colorsTypes/loadStyles.js
@@ -1,5 +1,5 @@
-import './colorsTypes-style.scss';
-import './colors/colors-style.scss';
+import './colorsTypes-style.scss'
+import './colors/colors-style.scss'
 import './colors/darkShade100/darkShade100%-style.scss'
 import './colors/darkShade75/darkShade75%-style.scss'
 import './colors/darkShade50/darkShade50%-style.scss'
