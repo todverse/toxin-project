6bdc868ce805d2fbf1e87f2a98249a285ae55d72
todverse <todverse0@gmail.com>
2021-11-08 11:59:16 +0400

    add dropDown

--- src/formElements/dropDown/dropDown.js
+++ src/formElements/dropDown/dropDown.js
@@ -1,9 +1,35 @@
 let dropDown = document.getElementsByClassName('formElements__dropDown_field')[0];
-console.log(dropDown.innerHTML);
+let hideElem = document.getElementsByClassName('formElements__dropDown_hideElem')[0];
+let buttonAdultPlus = document.getElementsByClassName('formElements__dropDown_hideElem_buttonCount')[0];
+let countAdult = 0;
+hideElem.style.display = 'none';
+hideElem.onmouseleave = function(event) {
+    dropDown.blur();
+};
+let adultPlus = () => {
+    countAdult += 1;
+    dropDown.focus();
+    if(countAdult > 1) {
+    dropDown.value = countAdult + ' взрослых';
+    } else dropDown.value = countAdult + ' взрослый';
+};
+let hide = () => {
+    hideElem.style.display = 'none';
+    dropDown.style.borderBottom = '';
+    dropDown.style.borderBottomRightRadius = '';
+    dropDown.style.borderBottomLeftRadius = '';
+    console.log('hide');
+    dropDown.removeEventListener('click', hide);
+    dropDown.addEventListener('click', show);
+};
 let show = () => {
-    dropDown.innerHTML = '50px';
-    console.log(dropDown.innerHTML);
+    hideElem.style.display = '';
+    dropDown.style.borderBottom = 'none';
+    dropDown.style.borderBottomLeftRadius = '0px';
+    dropDown.style.borderBottomRightRadius = '0px';
+    console.log('show');
     dropDown.removeEventListener('click', show);
-
+    dropDown.addEventListener('click', hide);
 }
-dropDown.addEventListener('click', show)
+dropDown.addEventListener('click', show);
+buttonAdultPlus.addEventListener('click', adultPlus);
