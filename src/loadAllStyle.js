28ed19c6a08eafbc8f92d567a84e15f55fb727ee
todverse <todverse0@gmail.com>
2021-11-17 13:03:23 +0400

    add airDatePicker

--- src/loadAllStyle.js
+++ src/loadAllStyle.js
@@ -1,3 +1,10 @@
 import './index.scss'
 import './formElements/dropDown/dropDown.js'
-
+import './formElements/dateDropDown/dateDropDown.js'
+import AirDatepicker from './../node_modules/air-datepicker/air-datepicker'
+new AirDatepicker('#input', {
+    buttons: ['clear', {content(dp) {return 'Применить'}}]
+});
+new AirDatepicker('#input1', {
+    buttons: ['clear', {content(dp) {return 'Применить'}}]
+});
